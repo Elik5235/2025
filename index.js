5bef19c936dac353e802c4539f3c8734d588426e
Elik <eliseyignatchenko06@gmail.com>
2025-03-13 12:28:16 +0200

    practise task resolve ternar

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const age = 10;
-const isAdult = age > 18 ? true : false;
-console.log(isAdult);
+// const isAdult = true;
+// const vievMode = isAdult ? true : false;
+// {
+//   if (isAdult) {
+//     console.log("ви авторизовані!");
+//   } else {
+//     console.log("ви не авторизовані!");
+//   }}
+const isAdult = true;
+const vievMode = isAdult ? 'authMode' : 'unauth';
+console.log(vievMode);
+
+
+
+
+const number = 11;
+const result = number >= 6 && number <= 12 ? 'in' : 'out';
+console.log(result);
