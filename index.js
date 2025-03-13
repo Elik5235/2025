5f3b7295dd4955350283d74d6f24d57da2f41c94
Elik <eliseyignatchenko06@gmail.com>
2025-03-13 12:57:03 +0200

    Switch-case welcome

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-// const isAdult = true;
-// const vievMode = isAdult ? true : false;
-// {
-//   if (isAdult) {
-//     console.log("ви авторизовані!");
-//   } else {
-//     console.log("ви не авторизовані!");
-//   }}
-const isAdult = true;
-const vievMode = isAdult ? 'authMode' : 'unauth';
-console.log(vievMode);
+// const number = Number(prompt("введіть число: "));
+// if (number % 5 === 0) {
+//   console.log(number + " ділиться на 5");
+// } else if (number % 3 === 0) {
+//   console.log(number + " ділиться на 3");
+// } else if (number % 2 === 0) {
+//   console.log(number + " ділиться на 2");
+// } else{
+//   console.log(number + ' число ні ділиться ні на 3, ні на 2, ні на 5');
+// };
 
-
-
-
-const number = 11;
-const result = number >= 6 && number <= 12 ? 'in' : 'out';
-console.log(result);
+const monthNumber = 11;
+switch (monthNumber) {
+  case 1:
+  case 2:
+  case 12:
+    console.log("зима");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("весна");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("літо");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log("осінь");
+    break;
+  default: {
+    console.log('некоректне значення, введіть від 1 до 12');
+  }
+}
