3972d0e155dbb10faa38066d8bc531575a12e6d3
Elik <eliseyignatchenko06@gmail.com>
2025-03-13 10:42:59 +0200

    isFunctionPractice

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const age = 9;
-const license = true;
-if(age >= 18 && license === true){
-    console.log('сідай за руль');
-}else if ( age < 18){
-    console.log('ви не можете, бо малий');
-}else if(license){
-   console.log('немає прав'); 
-}else{
-    console.log('помилка');
+function isFunctionPracticeGreeting(UserName) {
+  console.log('Привіт ,' + UserName);
 }
+
+isFunctionPracticeGreeting('Alex')
+isFunctionPracticeGreeting('Joel')
+isFunctionPracticeGreeting('Max')
+isFunctionPracticeGreeting('Tess')
