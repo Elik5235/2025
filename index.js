ce1fd0443d7422056492d8edd96f5851057ab01e
Elik <eliseyignatchenko06@gmail.com>
2025-03-13 22:49:43 +0200

    smart calculator

--- index.js
+++ index.js
@@ -1,63 +1,30 @@
-// const number = Number(prompt("введіть число: "));
-// if (number % 5 === 0) {
-//   console.log(number + " ділиться на 5");
-// } else if (number % 3 === 0) {
-//   console.log(number + " ділиться на 3");
-// } else if (number % 2 === 0) {
-//   console.log(number + " ділиться на 2");
-// } else{
-//   console.log(number + ' число ні ділиться ні на 3, ні на 2, ні на 5');
-// };
+// function sum(a,b){
+//     return a + b;
+// }
+// let superFunction = sum;
+// console.log(sum(2,4))
 
-const monthNumber = 11;
-switch (monthNumber) {
-  case 1:
-  case 2:
-  case 12:
-    console.log("зима");
-    break;
-  case 3:
-  case 4:
-  case 5:
-    console.log("весна");
-    break;
-  case 6:
-  case 7:
-  case 8:
-    console.log("літо");
-    break;
-  case 9:
-  case 10:
-  case 11:
-    console.log("осінь");
-    break;
-  default: {
-    console.log("некоректне значення, введіть від 1 до 12");
-  }
+function calculator(num1, num2, functionLink) {
+  let result = functionLink(num1, num2);
+  return result;
 }
-
-const menu = Number(prompt('1 - сік 2 - чай 3 - вода 4 - кола 5 - кава'));
-switch (menu) {
-  case 1: {
-    console.log('сік');
-    break;
-  }
-  case 2: {
-    console.log('чай');
-    break;
-  }
-  case 3: {
-    console.log('вода');
-    break;
-  }
-  case 4: {
-    console.log('кола');
-    break;
-  }
-  case 5: { 
-    console.log('кава');
-    break;
-  }
-  default: console.log('обкріть щось з меню');
+console.log(calculator(5, 5, multy));
+// Функция для сложения
+function add(a, b) {
+  return a + b;
 }
-
+// Функция для вычитания
+function subtract(a, b) {
+  return a - b;
+}
+// Функция для умножения
+function multy(a, b) {
+  return a * b;
+}
+// Функция для деления
+function divide(a, b) {
+  if (b === 0) {
+    return "Ошибка: деление на ноль!";
+  }
+  return a / b;
+}
