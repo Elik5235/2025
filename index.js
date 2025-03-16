af53743fc9414a67b699717aecd50d7a5481f169
Elik <eliseyignatchenko06@gmail.com>
2025-03-16 12:23:09 +0200

    do-while Practice

--- index.js
+++ index.js
@@ -1,47 +1,15 @@
-// console.log(1);
+// do(
+//     циклічний блок кода
+// )while(умова)
 
-//     let i = 0;
-//     while(i < 100){
-//         console.log(i);
-//         i++;
-//     }
-//     console.log(100);
+const correctPass = "qwerty123";
+let userPass;
+do {
+  userPass = prompt("Введіть пароль");
+} while (userPass !== correctPass);
 
-// while(true){
-// console.log('не вірно');
-//     break;
-// }
-
-// let i = 0;
-// while (i < 10) {
-//   if (i % 2 === 0) {
-//     console.log(i);
-//   }
-//   i++;
-// }
-
-const password = "qwerty123";
-let userPassword = prompt("Введіть пароль");
-while (userPassword !== password) {
-  userPassword = prompt("Введіть вірний пароль");
+if (userPass === correctPass) {
+  alert("Ласкаво просимо");
+} else {
+  alert("Пароль невірний");
 }
-alert("Ласкаво просимо");
-
-// var 2
-while (true) {
-  const userPass = prompt("Введіть пароль");
-  if (userPass === correctPassword) {
-    break;
-  }
-}
-
-//var 3
-let isPasswordCorrect = false;
-while (!isPasswordCorrect) {
-    const userPass = prompt("Введіть пароль");
-    if (userPass === correctPassword) {
-        isPasswordCorrect = true;
-    }
-}
-
-
