db936055328d039c24e7601b86cf8c5cf695b1e5
Elik <eliseyignatchenko06@gmail.com>
2025-03-16 12:28:09 +0200

    poor for

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// // for
-// for(let i = 0; i < 10; i++){
-//     console.log(i)
-// }
-
-// // while
-// let i = 0
-// while(i < 10){
-//     console.log(i)
-//     i++
-// }
-
-// // do while
-// let j = 0
-// do{
-//     console.log(j)
-//     j++
-// }while(j < 10)
-    
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
