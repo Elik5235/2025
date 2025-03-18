0e830049bc97607a5500c670f32d9b3c513beee0
Elik <eliseyignatchenko06@gmail.com>
2025-03-18 22:18:52 +0200

    fizzBuzzGame

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-function diap(a, b) {
-  for (let i = a; i <= b; i++) {
-    if (i % 5 === 0) {
-      console.log(i);
+function fizzBuzzGame(limit){
+    for (let i = 1; i <= limit; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
     }
-  }
 }
-diap(10, 50);
-function count() {
-  let num1 = Number(prompt("Введите первое число"));
-  let num2 = Number(prompt("Введите второе число"));
-  if(num1 >num2){
-    console.log('1 число повинно бути менше за 2');
-    return;
-  }
-  for (let i = num1; i <= num2; i++) {
-    if (i % 5 === 0) {
-      console.log(i);
-    }else{
-        console.log('Немає числа що ділиться на 5');
-    }
-  }
-}
-count()
+fizzBuzzGame(100)
