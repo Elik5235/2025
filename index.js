e6ba47907d3e9f1b3397df12e970002b43dbd1d4
Elik <eliseyignatchenko06@gmail.com>
2025-03-18 21:12:10 +0200

    Factorial(for)

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-for(let i = 0; i < 10; i++){
-    console.log(i);
+function factorial(num) {
+  let res = 1;
+
+  for (let it = 1; it <= num; it++) {
+    if (num === 0 || num === 1) {
+      return 1;
+    } else if (num < 0) {
+      return null;
+    } else {
+      res = res * it;
+    }
+  }
+  return res;
 }
+console.log(factorial(5));
