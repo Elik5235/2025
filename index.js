21de41aa4fb54ea882d1af4d1c7fb726789b4824
Elik <eliseyignatchenko06@gmail.com>
2025-03-19 00:07:12 +0200

    Book Shop Example

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-// const bulb = {
-//   lightness: 200,
-//   power: "200w",
-//   cap: "small",
-//   on: function () {
-//     return "light on!";
-//   },
-//   off: function () {
-//     return "Light off!";
-//   },
-// };
+function book(title, author, year, price) {
+  (this.title = title),
+    (this.author = author),
+    (this.year = year),
+    (this.price = price),
+    (this.GetTitle = function () {
+      return this.title;
+    });
+  this.GetAuthor = function () {
+    return this.author;
+  };
+  this.GetYear = function () {
+    return this.year;
+  };
+  this.GetPrice = function () {
+    return this.price;
+  };
+  this.setTitle = function (newTitle) {
+    this.title = newTitle;
+  };
+  this.setAuthor = function (newAuthor) {
+    this.title = newAuthor;
+  };
+  this.setYear = function (newYear) {
+    this.title = newYear;
+  };
+  this.setPrice = function (newPrice) {
+    this.title = newPrice;
+  };
 
-
-// const cat = {};
-// const cat2 = Object();
-// const cat3 = new Object();
-
-
-const cat ={
-    name:'cat',
-    color:'red',
-    breed: 'seam',
-    age:'2',
-    eat:function(){
-        return 'я куша.'
-    },
-    sleep:function(){
-        return 'я сплю'
-    }
+  
+  this.calculatorDiscount = function(Discount){
+    const DiscountPrice = this.price - (this.price * (Discount / 100))
+    return DiscountPrice;
+  }
 }
+const book1 = new book('FIVE hands','Agata Kristi','1999','1221$')
+// console.log(book1.GetAuthor(),book1.GetPrice(),book1.GetTitle(),book1.GetYear());
+
+book1.setTitle('New Title');
