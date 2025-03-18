7a828acccca4368707f646c15c75f3ac684a3768
Elik <eliseyignatchenko06@gmail.com>
2025-03-19 00:43:50 +0200

    practise task resovle 2

--- index.js
+++ index.js
@@ -1,51 +1,25 @@
-// function book(title, author, year, price) {
-//   (this.title = title),
-//     (this.author = author),
-//     (this.year = year),
-//     (this.price = price),
-//     (this.GetTitle = function () {
-//       return this.title;
-//     });
-//   this.GetAuthor = function () {
-//     return this.author;
-//   };
-//   this.GetYear = function () {
-//     return this.year;
-//   };
-//   this.GetPrice = function () {
-//     return this.price;
-//   };
-//   this.setTitle = function (newTitle) {
-//     this.title = newTitle;
-//   };
-//   this.setAuthor = function (newAuthor) {
-//     this.title = newAuthor;
-//   };
-//   this.setYear = function (newYear) {
-//     this.title = newYear;
-//   };
-//   this.setPrice = function (newPrice) {
-//     this.title = newPrice;
-//   };
+function Auto(name, maxSpeed, speed) {
+  this.name = name;
+  this.maxSpeed = maxSpeed;
+  this.speed = 0;
 
-//   this.calculatorDiscount = function(Discount){
-//     const DiscountPrice = this.price - (this.price * (Discount / 100))
-//     return DiscountPrice;
-//   }
-// }
-// const book1 = new book('FIVE hands','Agata Kristi','1999','1221$')
-// console.log(book1.GetAuthor(),book1.GetPrice(),book1.GetTitle(),book1.GetYear());
-
-// book1.setTitle('New Title');
-
-function Country(NameCountry, population, area) {
-  (this.NameCountry = NameCountry),
-    (this.population = population),
-    (this.area = area);
-
-  this.GetDensity = function () {
-    return this.population / this.area;
+  this.accelerate = function (accelValue) {
+    this.speed = this.speed + accelValue;
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    return this.speed;
   };
+  this.deaccelerate = function (deaccelValue) {
+    this.speed = this.speed - deaccelValue;
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+    return this.speed;
+  };
+  this.stop = function(){
+    this.speed = 0;
+  }
+  return this.speed
 }
-
-const UK = new Country('Uk','67333000',243610)
+const auto1 = new Auto('Mercedes',303)
