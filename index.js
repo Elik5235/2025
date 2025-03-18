f6f929b16331dfba252991da3f6f5eb278b3fa92
Elik <eliseyignatchenko06@gmail.com>
2025-03-19 01:04:15 +0200

    template strings (`${  }`)

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-function Auto(name, maxSpeed, speed) {
-  this.name = name;
-  this.maxSpeed = maxSpeed;
-  this.speed = 0;
+const name = 'Vasya'
+// alert(`Hello, ${name}!`)
 
-  this.accelerate = function (accelValue) {
-    this.speed = this.speed + accelValue;
-    if (this.speed > this.maxSpeed) {
-      this.speed = this.maxSpeed;
-    }
-    return this.speed;
-  };
-  this.deaccelerate = function (deaccelValue) {
-    this.speed = this.speed - deaccelValue;
-    if (this.speed < 0) {
-      this.speed = 0;
-    }
-    return this.speed;
-  };
-  this.stop = function(){
-    this.speed = 0;
-  }
-  return this.speed
-}
-const auto1 = new Auto('Mercedes',303)
+// alert(`Result ${1 + 1}`)
+//  !!! `` !!!
+
+const first = Number(prompt('first Num:'))
+const last = Number(prompt('last Num:'))
+alert(`${first + last}!`)
