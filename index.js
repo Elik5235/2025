95eb5eba6501778b8a92e9a1682f7c8b7668d184
Elik <eliseyignatchenko06@gmail.com>
2025-03-19 22:58:07 +0200

    Practuse task resolve

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const greets = {
-    15:'HI',
-    17:'Whats up',
-    19:'Hello',
-    30:'aloha'
-}
-
-const valueFromUser = 19;
-console.log(greets[valueFromUser]);
+const obj = {
+  Mon: "drive lesson",
+  Tue: "dantist visit",
+  Wed: "go to party",
+  Thu: "hard work",
+  Fri: "chill",
+  Sat: "go to park",
+};
+const userValue = prompt("Enter your day");
+alert(obj[userValue] ? obj[userValue] : "Error");
