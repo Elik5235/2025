775d6ffbdd41604aae14cb6ec00b6877e3f3889b
Elik <eliseyignatchenko06@gmail.com>
2025-03-23 14:05:48 +0200

    sumOfWasteSalary

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const obj = {
-  Mon: "drive lesson",
-  Tue: "dantist visit",
-  Wed: "go to party",
-  Thu: "hard work",
-  Fri: "chill",
-  Sat: "go to park",
+const departmentSalary = {
+  HR: 120000,
+  development: 5500000,
+  finance: 1500000,
+  marketing: 1000000,
+  sales: undefined,
 };
-const userValue = prompt("Enter your day");
-alert(obj[userValue] ? obj[userValue] : "Error");
+
+function sumOfWasteSalary(departmentSalary) {
+  let total = 0;
+  let nameOfUndefined;
+
+  for (const key in departmentSalary) {
+    total += departmentSalary[key];
+    if (typeof departmentSalary[key] === "number") {
+      total += departmentSalary[key];
+    } else {
+      nameOfUndefined = key;
+      console.log(`Немає вірного значення: ${nameOfUndefined}`);
+    }
+    return total;
+  }
+}
+console.log(sumOfWasteSalary(departmentSalary));
