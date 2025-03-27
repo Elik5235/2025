8f0b1070e2207393fbaa56043b8f8317f8e03b4e
Elik <eliseyignatchenko06@gmail.com>
2025-03-27 15:06:10 +0200

    chaining

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// const userTelephone = {
-//   0:'380977973709',
-//   1:'380977973854',
-//   2:'380977973565',
-//   3:'380977973433',
+const array = [2, 3, 4, 5, 1, 2];
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] % 2 === 0) {
-    console.log(arr[i]);
-  }
-}
+const newArray = array
+  .map((item) => {
+    return item * 2;
+  })
+  .reverse().join(',');
