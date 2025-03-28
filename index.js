e9f0bfaedb52a817f40fea0322cd6bd41a09c0aa
Elik <eliseyignatchenko06@gmail.com>
2025-03-28 15:10:03 +0200

    This in method

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const cat = {
-  name: 'Barsik',
-  color: 'red',
-  age: 1
-}
-
-const cat2 = {
-  name: 'Murzik',
-  color: 'black',
-  age: 5
-}
-
-const catMethods = { // прототип
-  run: function() {
-    console.log('Cat is running');
+const catMethods = {
+  // прототип
+  run: function () {
+    console.log(`${this.name} is running!`);
   },
-  meow: function() {
-    console.log('Meow!');
-  }
-}
-
-// Як прикрутити catMethods до об'єкту cat?
-
-cat.__proto__ = catMethods; // прототипне посилання
-cat2.__proto__ = catMethods;
+  meow: function () {
+    console.log(`${this.name} said meow!`);
+  },
+};
