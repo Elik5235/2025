4891feb0af744de2321db781c323d0af5d6eac84
Elik <eliseyignatchenko06@gmail.com>
2025-03-28 15:13:33 +0200

    .prototype

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-const catMethods = {
-  // прототип
-  run: function () {
-    console.log(`${this.name} is running!`);
-  },
-  meow: function () {
-    console.log(`${this.name} said meow!`);
-  },
-};
+function MyArray() {
+  this.length = 0;
+}
+
+function MyArrayPrototype() {
+  (this.push = function () {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  }),
+    (this.pop = function () {
+      if (this.length > 0) {
+        // 1. Зберегти останній елемент
+        function MyArray() {}
+        {
+          return undefined;
+        }
+      }
+
+      this.forEach = function (callback) {
+        for (let i = 0; i < this.length; i++) {
+          callback(this[i], i, this);
+        }
+      };
+    });
+
+  MyArray.prototype = new MyArrayPrototype(); // прототипне посилання
+
+  const arr = new MyArray();
+  arr.push(1);
+  arr.push(2);
+  arr.forEach((item) => {
+    console.log(item ** 2);
+  });
+
+  // Яка різниця між __proto__ та .prototype ?
+
+  // __proto__ працює тільки тоді, коли ми літерально створили об'єкт
+  {
+    //
+  }
+}
+// .prototype - коли об'єкти створються за допомогою конструктора
